78f0eb073f94c3f0f6e44a15773d9ef030a7f7d8
samsonites <samsam.son@samsung.com>
2017-01-10 22:54:07 +0900

    creating PracticeOne java file

---
+++ src/com/samsamson/practice/PracticeOne.java
@@ -0,0 +1,17 @@
+package com.samsamson.practice;
+
+/**
+ * Created by 손승원 on 2017-01-10.
+ */
+public class PracticeOne {
+    public static void main(String[]args) {
+        int i = 0;
+        System.out.println(i);
+    }
+
+
+
+}
+
+
+
